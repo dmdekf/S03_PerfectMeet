79c997bdca70dad41dbd1d9d0732a67f72a280e2
Kang Myun Hun <lizingmaster@gmail.com>
2020-11-10 14:22:46 +0900

    StoreRes 수정
    - Request 항목변경
    - 등록 로직 변경

--- backend/MatNam/src/main/java/com/POM/MatNam/storeres/Controller/StoreResController.java
+++ backend/MatNam/src/main/java/com/POM/MatNam/storeres/Controller/StoreResController.java
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.POM.MatNam.Board.DAO.BoardDao;
-import com.POM.MatNam.Board.DTO.Board;
-import com.POM.MatNam.Board.Response.BasicResponse;
+import com.POM.MatNam.response.BasicResponse;
+import com.POM.MatNam.response.ErrorResponse;
 import com.POM.MatNam.storeres.DAO.StoreResDao;
 import com.POM.MatNam.storeres.DTO.StoreRes;
 import com.POM.MatNam.storeres.DTO.StoreResRequest;
+import com.POM.MatNam.user.dao.UserDao;
+import com.POM.MatNam.user.dto.User;
+import com.POM.MatNam.user.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -35,6 +35,11 @@ public class StoreResController {
    @Autowired
    StoreResDao storeResDao;
   
+   @Autowired
+   UserService userService;
+   
+   @Autowired
+   UserDao userDao;
    
 //   @ApiOperation(value="수정하기", response = StoreResController.class)
 //   @PutMapping("/update")
@@ -58,7 +63,7 @@ public class StoreResController {
 //      return response;
 //   }
 	
-	@ApiOperation(value = "모든 게시글의 정보를 반환한다.", response = List.class)
+	@ApiOperation(value = "모든 예약 가능한 가게 정보를 반환한다.", response = List.class)
 	@GetMapping("/list")
 	public List<StoreRes> getStoreResList() throws Exception {
 		
@@ -78,16 +83,35 @@ public class StoreResController {
 //		
 //	}
 
-	@ApiOperation(value = "새로운 가게정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "새로운 가게정보를 입력한다.", response = String.class)
 	@PostMapping("/write")
 	public Object addStoreRes(@RequestBody StoreResRequest storeres) {
-		System.out.println("가게 정보 삽입 진입 성공");
+		ResponseEntity<BasicResponse> response = null;
+		Map<String, Object>errors = new HashMap<>();
+		
 		StoreRes temp = new StoreRes();
 		temp.setAddress(storeres.getAddress());
 		temp.setName(storeres.getName());
 		temp.setTel(storeres.getTel());
-		return storeResDao.save(temp);
 		
+		User s_User = userService.selectByNickname(storeres.getNickname());
+		
+		if(s_User==null) {
+			errors.put("field", "addStoreUser");
+			errors.put("nickname", storeres.getNickname());
+			final ErrorResponse result = setErrors("E-4700", "가게 주인으로 등록하고자 하는 유저가 없습니다.", errors);
+			response = new ResponseEntity<BasicResponse>(result, HttpStatus.NOT_FOUND);
+		}else {
+			final BasicResponse result = new BasicResponse();
+			StoreRes s_temp = storeResDao.save(temp);
+			s_User.setStore_id(s_temp.getId());
+			userDao.save(s_User);
+			result.status = "200";
+			result.message="메세지 삭제 성공";
+			response = new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+		}
+		
+		return response;
 	}
 
 	@ApiOperation(value = "가게 아이디에 해당하는 정보를 삭제한다.", response = String.class)
@@ -99,5 +123,12 @@ public class StoreResController {
 		return null;
 	}
 	
+	private ErrorResponse setErrors(String status, String message, Map<String, Object> errors) {
+		ErrorResponse res = new ErrorResponse();
+		res.status = status;
+		res.message = message;
+		res.errors = errors;
+		return res;
+	}
 
 }
