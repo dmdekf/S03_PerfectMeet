e0cae80cba1018f170d60d872c52b6ebce5b4bf5
Kang Myun Hun <lizingmaster@gmail.com>
2020-11-11 16:55:20 +0900

    message delete add

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,17 @@ import store from "./store";
 Vue.config.productionTip = false
 
 import vuetify from './plugins/vuetify';
+import VuetifyDialog from 'vuetify-dialog'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VuetifyDialog,{
+  context:{
+    vuetify
+  }
+})
+
 new Vue({
   store,
   router,
